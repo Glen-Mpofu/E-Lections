1ef0b6c8b4d6f767767ded430dd35c7afb207355
Glen Mpofu <mgthades2004@gmail.com>
2025-10-26 19:16:22 +0200

    updated the cors

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const pool = new Pool({
 });
 
 app.use(cors({
-    origin: ['http://localhost:8081', 'https://e-lections-production.up.railway.app'],
+    origin: ['http://localhost:8081'],
     credentials: true,
 }));
 pool.connect()
